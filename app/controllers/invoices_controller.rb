dfb4918ed17ed481e25eb30d27201684ff85b445
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-02-15 01:10:31 +0100

    Fix company name returning null

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -2,7 +2,7 @@ class InvoicesController < ApplicationController
   def generate
     invoices, total_cost = InvoiceService.new(timesheets).invoices_and_total_cost
 
-    render json: { invoices: invoices, total_cost: total_cost, employee_name: params[:employee_name] }
+    render json: { invoices: invoices, total_cost: total_cost, company_name: params[:company_name] }
   end
 
   private
