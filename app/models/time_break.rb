02083e0e484d830a58a0a524472121c43ae218d5
Oluwakunle Fakorede <fakoredeoluwakunle2626@gmail.com>
2021-02-25 16:37:15 +0100

    Optimise code

--- app/models/time_break.rb
+++ app/models/time_break.rb
@@ -4,12 +4,14 @@ class TimeBreak < ApplicationRecord
   validate :validate_time_overlap
   validates :name, :start_time, :end_time, presence: true
 
+  scope :timebreaks_for_a_timesheet, -> (timesheet_id) { includes(:time_sheet).where(time_sheet: { id: timesheet_id }) }
+
   private
 
   def validate_time_overlap
     return self.errors.add(:base, "Time breaks must be between working hours") unless between_work_hours.overlaps?(new_break_slot)
 
-    overlap_timebreak = time_sheet.time_breaks.any? {|time_break| new_break_slot.overlaps?(time_break.start_time..time_break.end_time) }
+    overlap_timebreak = TimeBreak.timebreaks_for_a_timesheet(timesheet.id).any? {|time_break| new_break_slot.overlaps?(time_break.start_time..time_break.end_time) }
     self.errors.add(:base, "Time breaks can't overlap") if overlap_timebreak
   end
 
